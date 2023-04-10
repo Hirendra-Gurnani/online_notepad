9650571f4fa3a9ea772b88f9335785f5d5f70d29
Hirendra-Gurnani <92821585+Hirendra-Gurnani@users.noreply.github.com>
2023-04-10 12:40:52 +0530

    fix: Applied fixes to front end elements and styled

--- src/components/notePad.js
+++ src/components/notePad.js
@@ -36,11 +36,15 @@ function Notepad() {
   };
 
   const handleSave = () => {
-    const fileText = value;
-    // const fileName = "my-notes.txt";
-    const blob = new Blob([fileText], { type: "text/plain;charset=utf-8" });
-    saveAs(blob, `${fileName}.txt`);
-    fileDownload(fileText, `${fileName}.txt`);
+    if (fileName === "" || fileName === null || fileName === 0) {
+      window.alert("Please mention a filename");
+    } else {
+      const fileText = value;
+      // const fileName = "my-notes.txt";
+      const blob = new Blob([fileText], { type: "text/plain;charset=UTF-8" });
+      saveAs(blob, `${fileName}.txt`);
+      fileDownload(fileText, `${fileName}.txt`);
+    }
   };
 
   const getStyle = () => {
@@ -61,61 +65,68 @@ function Notepad() {
 
   return (
     <>
-    <div className="App App-header"><h1>Notepad Online</h1></div>
-      <div className="notepad">
-        <div className="notepad-field">
-          {/* <label htmlFor="filename">Filename:</label> */}
-          <input
-            id="filename"
-            type="text"
-            placeholder="Title Name Here..."
-            value={fileName}
-            onChange={(e) => setFileName(e.target.value)}
-          />
-        </div>
-        <div className="toolbar">
-          <select value={fontFamily} onChange={handleFontFamilyChange}>
-            <option value="Arial">Arial</option>
-            <option value="Helvetica">Helvetica</option>
-            <option value="Times New Roman">Times New Roman</option>
-            <option value="Georgia">Georgia</option>
-            <option value="Courier New">Courier New</option>
-            <option value="Verdana">Verdana</option>
-            <option value="Impact">Impact</option>
-            <option value="Open Sans">Open Sans</option>
-            <option value="Montserrat">Montserrat</option>
-            <option value="Roboto">Roboto</option>
-            <option value="Lato">Lato</option>
-          </select>
-          <select value={fontSize} onChange={handleFontSizeChange}>
-            <option value="12px">12</option>
-            <option value="14px">14</option>
-            <option value="16px">16</option>
-            <option value="18px">18</option>
-            <option value="20px">20</option>
-            <option value="22px">22</option>
-            <option value="24px">24</option>
-          </select>
-          <div className="bttnStyle">
-            <button onClick={handleBoldClick} className={bold ? "active" : ""}>
-              <b>B</b>
-            </button>
-            <button
-              onClick={handleItalicClick}
-              className={italic ? "active" : ""}
-            >
-              <i>I</i>
-            </button>
-            <button
-              onClick={handleUnderlineClick}
-              className={underline ? "active" : ""}
-            >
-              <u>U</u>
-            </button>
+      <div className="App App-header">
+        <h1>Notepad Online</h1>
+      </div>
+      <form>
+        <div className="notepad">
+          <div className="notepad-field">
+            <input
+              id="filename"
+              type="text"
+              placeholder="Title Name Here..."
+              value={fileName}
+              onChange={(e) => setFileName(e.target.value)}
+              required
+              title="Please give a name for the file here"
+            />
           </div>
-        </div>
-        <hr />
-        <form>
+          <div className="toolbar">
+            <select value={fontFamily} onChange={handleFontFamilyChange}>
+              <option value="Arial">Arial</option>
+              <option value="Helvetica">Helvetica</option>
+              <option value="Times New Roman">Times New Roman</option>
+              <option value="Georgia">Georgia</option>
+              <option value="Courier New">Courier New</option>
+              <option value="Verdana">Verdana</option>
+              <option value="Impact">Impact</option>
+              <option value="Open Sans">Open Sans</option>
+              <option value="Montserrat">Montserrat</option>
+              <option value="Roboto">Roboto</option>
+              <option value="Lato">Lato</option>
+            </select>
+            <select value={fontSize} onChange={handleFontSizeChange}>
+              <option value="12px">12</option>
+              <option value="14px">14</option>
+              <option value="16px">16</option>
+              <option value="18px">18</option>
+              <option value="20px">20</option>
+              <option value="22px">22</option>
+              <option value="24px">24</option>
+            </select>
+            <div className="bttnStyle">
+              <button
+                onClick={handleBoldClick}
+                className={bold ? "active" : ""}
+              >
+                <b>B</b>
+              </button>
+              <button
+                onClick={handleItalicClick}
+                className={italic ? "active" : ""}
+              >
+                <i>I</i>
+              </button>
+              <button
+                onClick={handleUnderlineClick}
+                className={underline ? "active" : ""}
+              >
+                <u>U</u>
+              </button>
+            </div>
+          </div>
+          <hr />
+          {/* <form> */}
           <textarea
             style={getStyle()}
             value={value}
@@ -125,8 +136,9 @@ function Notepad() {
           <button onClick={handleSave} type="submit">
             Save
           </button>
-        </form>
-      </div>
+          {/* </form> */}
+        </div>
+      </form>
     </>
   );
 }
